f3c11735985abc8ad27d7382e413faf864e35aed
o0pmitev <pmitev89@gmail.com>
2020-06-13 15:46:32 +0200

    Add comments and more emails

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Using nodemailer for sending emails: 
+// https://nodemailer.com/
 const nodemailer = require('nodemailer');
+
+// A json file with fake emails
 const emails = require('./fake_mails.json');
+let arrayOfEmails = emails.map(item => item.email);
+console.log(`Emails: ${arrayOfEmails.length}`);
 
-let arrayOfEmails = emails.map(item => {
-  return item.email
-})
-
+// A transporter object 
 const transporter = nodemailer.createTransport({
+  pool: true,
   maxMessages: Infinity,
   maxConnections: 20,
   host: "smtp.mailtrap.io",
@@ -16,23 +20,26 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+// Email
 var mailOptions = {
   from: 'mitev.berlin@gmail.com',
   to: arrayOfEmails,
-  subject: 'Ha it works',
-  text: 'Hello',
-  html: `
-  <h1>Plamen 4 was here </h1>
-  <p>and left you a message</p>
+  subject: 'Fake Email',
+  text: 'Fake emails from Plamen Mitev',
+  html: 
+  `
+  <h1>Plamen Mitev</h1>
+  <p>30 years old</p>
   
   `
 };
 
+// Sending emails
 transporter.sendMail(mailOptions, function(error, info) {
   if(error) {
     console.log(error);
   } else {
     console.log(`Email sent:  ${info.response}`);
-  }
-})
+  };
+});
 
